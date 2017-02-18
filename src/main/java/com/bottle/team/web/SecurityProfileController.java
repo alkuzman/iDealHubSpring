f774928619359932ab6b9205f4d5d72a961ab6f0
VikiPeeva <peeva_viki@yahoo.com>
2017-02-18 16:44:21 +0100

    #637922/Return certificate for user

--- src/main/java/com/bottle/team/web/SecurityProfileController.java
+++ src/main/java/com/bottle/team/web/SecurityProfileController.java
@@ -6,16 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Viki on 2/18/2017.
  */
+@RestController
+@RequestMapping(value = "/securityprofiles")
 public class SecurityProfileController {
 
     @Autowired
     public SecurityProfileService securityProfileService;
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public SecurityProfile save(@RequestBody SecurityProfile profile) {
         return securityProfileService.save(profile);
     }
