58086c8763e12bb2db51475fbee0dac92e85b1b3
VikiPeeva <peeva_viki@yahoo.com>
2017-03-09 13:57:06 +0100

    #637925/Create M-TWO

--- src/main/java/com/bottle/team/model/security/ProtocolTransactionStep.java
+++ src/main/java/com/bottle/team/model/security/ProtocolTransactionStep.java
@@ -1,10 +1,12 @@
 package com.bottle.team.model.security;
 
 import com.bottle.team.model.BaseEntityImpl;
+import com.bottle.team.model.authentication.Agent;
 import com.bottle.team.model.enumaration.ProtocolTransactionMessageNumber;
 import com.oracle.webservices.internal.api.message.PropertySet;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Property;
+import org.neo4j.ogm.annotation.Relationship;
 
 /**
  * Created by Viki on 3/2/2017.
@@ -16,6 +18,9 @@ public class ProtocolTransactionStep extends BaseEntityImpl {
 
     @Property(name = "stepMessage")
     private String stepMessage;
+
+    @Relationship(type = "CREATOR")
+    private Agent creator;
 
     public ProtocolTransactionMessageNumber getStepNumber() {
         return stepNumber;
@@ -31,5 +36,13 @@ public class ProtocolTransactionStep extends BaseEntityImpl {
 
     public void setStepMessage(String stepMessage) {
         this.stepMessage = stepMessage;
+    }
+
+    public Agent getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Agent creator) {
+        this.creator = creator;
     }
 }
